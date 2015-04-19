02ad8c4d71a5a1dae35c6c92227da2a78431134c
Wake Liu <wake.gs@gmail.com>
2015-04-20 01:12:55 +0800

    修正 Plugin 說明，並修改 rss feed 中的 guid 為 permalink

--- wp-remove-shortlink.php
+++ wp-remove-shortlink.php
@@ -1,13 +1,21 @@
 <?php
 
 /*
-Plugin Name: Shortlink Removal
-Plugin URI: http://www.douglasradburn.co.uk/
-Description: Remove shortlink hook
+Plugin Name: WP Remove Shortlink
+Plugin URI: http://wake.gs
+Description: 同時移除文章內的 short link 並調整 rss feed 中的 guid
 Version: 1.0
-Author: Douglas Radburn
-Author URI: http://www.douglasradburn.co.uk
+Author: Wake
+Author URI: http://wake.gs
 */
 
-remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+remove_action ('wp_head', 'wp_shortlink_wp_head', 10, 0);
+
+
+add_filter ('get_the_guid', 'use_guid_with_permalink');
+
+function use_guid_with_permalink ($guid) {
+  return get_permalink ();
+}
+
 ?>
